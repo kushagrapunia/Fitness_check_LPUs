3d1b49edad84ff9e9d72f335787f4f347b583a1f
Kushagra Punia <kushagrapunia@gmail.com>
2023-06-15 15:56:43 +0530

    ok

--- cpu_details.py
+++ cpu_details.py
@@ -1,17 +1,24 @@
 import subprocess
 
 #CPU reading might not be accurate
-def get_CPU_temprature():   
+def get_CPU_temperature():   
     temp_output = subprocess.check_output(['sensors']).decode('utf-8')
-    cpu_temp = temp_output.strip().split('\n')[2].split()[1]
-    return cpu_temp
+    return temp_output.strip().split('\n')[2].split()[1]
 
 
 
 def get_CPU_usage():
-    GPU_usage = round(100 - float(subprocess.check_output(['top', '-n', '1']).decode('utf-8').split("\n")[2].split()[7]), 3)
-    return GPU_usage
+    return round(
+        100
+        - float(
+            subprocess.check_output(['top', '-n', '1'])
+            .decode('utf-8')
+            .split("\n")[2]
+            .split()[7]
+        ),
+        3,
+    )
 
 
-print('CPU Usage: {}'.format(get_CPU_usage()))
-print('CPU Temperature: {}°C'.format(get_CPU_temprature()))
+print(f'CPU Usage: {get_CPU_usage()}')
+print(f'CPU Temperature: {get_CPU_temperature()}')
